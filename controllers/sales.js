ba3cd0c6f9544d19e84eb673c8e482a52fd008f0
furquin <laert.ff@gmail.com>
2022-03-30 23:54:14 -0300

    feat: cria função para lidar com requisição de venda por id

--- controllers/sales.js
+++ controllers/sales.js
@@ -6,6 +6,15 @@ const getAllSales = async (req, res) => {
     res.status(200).json(allSales);
 };
 
+const getSaleById = async (req, res) => {
+  const { id } = req.params;
+  const saleById = await serviceSales.getSaleById(id);
+
+  if (saleById[0]) {
+    res.status(200).json(saleById);
+  }
+};
 module.exports = {
   getAllSales,
+  getSaleById,
 };
