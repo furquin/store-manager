17d20bcf9cdad7252e1b5f0cc29980b556988406
furquin <laert.ff@gmail.com>
2022-03-30 23:36:18 -0300

    feat: usa rota de produtos no index

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+const express = require('express');
+const productsRoutes = require('./routes/products');
 require('dotenv').config();
 
-// não remova esse endpoint, e para o avaliador funcionar
+const app = express();
+app.use(express.json());
+
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.use('/products', productsRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
