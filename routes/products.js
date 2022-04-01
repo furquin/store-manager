c878a251f26c299f9dd4c0fd299a0a47a2ec4184
furquin <laert.ff@gmail.com>
2022-03-31 23:50:24 -0300

    feat: implementa rota de atualização com midleWares de verificação

--- routes/products.js
+++ routes/products.js
@@ -3,11 +3,13 @@ const {
   getAllProducts,
   getByIdProducts,
   createProducts,
+  updateProduct,
 } = require('../controllers/products');
 const {
   checkNameExist,
   checkName,
   checkQuantity,
+  checkById,
 } = require('../middlewares/checkProducts');
 
 const routes = express.Router();
@@ -17,7 +19,9 @@ routes
   .post(checkNameExist, checkName, checkQuantity, createProducts)
   .get(getAllProducts);
 
-routes.route('/:id')
-  .get(getByIdProducts);
+routes
+  .route('/:id')
+  .get(checkById, getByIdProducts)
+  .put(checkById, checkName, checkQuantity, updateProduct);
 
 module.exports = routes;
