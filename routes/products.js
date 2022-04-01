d128518a7bc37e5bc9f75a3272c1eb0a80857a68
furquin <laert.ff@gmail.com>
2022-03-31 22:28:04 -0300

    feat: implementa verificação na rota de produtos

--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,20 @@
 const express = require('express');
-const { getAllProducts, getByIdProducts } = require('../controllers/products');
-const { checkName, checkQuantity } = require('../middlewares/checkProducts');
+const {
+  getAllProducts,
+  getByIdProducts,
+  createProducts,
+} = require('../controllers/products');
+const {
+  checkNameExist,
+  checkName,
+  checkQuantity,
+} = require('../middlewares/checkProducts');
 
 const routes = express.Router();
 
-routes.route('/')
-  .post(checkName, checkQuantity)
+routes
+  .route('/')
+  .post(checkNameExist, checkName, checkQuantity, createProducts)
   .get(getAllProducts);
 
 routes.route('/:id')
