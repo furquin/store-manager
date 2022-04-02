a27bf93b2713525dea9541d71e7251c5503a9dc0
furquin <laert.ff@gmail.com>
2022-04-01 22:55:59 -0300

    refactor: implementas funções para atualizar quantity em sale ou delete sale

--- services/sales.js
+++ services/sales.js
@@ -1,4 +1,5 @@
 const modelSales = require('../models/sales');
+const modelProducts = require('../models/products');
 
 const getAllSales = async () => {
   const allSales = await modelSales.getAllSales();
@@ -13,7 +14,11 @@ const getSaleById = async (id) => {
 };
 
 const createSales = async (sales) => {
+  const [{ productId, quantity }] = sales;
+
   const newSales = await modelSales.createSales(sales);
+
+  await modelProducts.updateQuantitySale(productId, quantity);
 
   return newSales;
 };
@@ -25,7 +30,12 @@ const updateSales = async (id, productId, quantity) => {
 };
 
 const deleteSales = async (id) => {
+  const saleById = await modelSales.getSaleById(id);
+  const [{ productId, quantity }] = saleById;
+
   const deleted = await modelSales.deleteSales(id);
+
+  await modelProducts.updateQuantityDelete(productId, quantity);
 
   return deleted;
 };
